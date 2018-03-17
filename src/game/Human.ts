d9c5d4ec271925bc6ed6a979f75516c60e970a86
pierallard <pierallard@gmail.com>
2018-03-17 13:01:35 +0100

    fuse images

--- src/game/Human.ts
+++ src/game/Human.ts
@@ -1,10 +1,7 @@
 import {PositionTransformer} from "./PositionTransformer";
 
 export class Human {
-    private tileWalk: Phaser.TileSprite;
-    private tileWalkReverse: Phaser.TileSprite;
-    private tileDefault: Phaser.TileSprite;
-    private tileDefaultReverse: Phaser.TileSprite;
+    private tile: Phaser.TileSprite;
     private cell: PIXI.Point;
     private game: Phaser.Game;
     private isMoving: boolean;
@@ -20,33 +17,13 @@ export class Human {
         this.isLeft = false;
         this.isTop = false;
 
-        this.tileWalk = game.add.tileSprite(realX, realY, 24, 24, 'walk');
-        this.tileWalk.animations.add('walk');
-        this.tileWalk.animations.play('walk', 12, true);
-        this.tileWalk.alpha = 0;
-        this.tileWalk.anchor.set(0.5, 1.3);
-        game.add.existing(this.tileWalk);
-
-        this.tileDefault = game.add.tileSprite(realX, realY, 24, 24, 'default');
-        this.tileDefault.animations.add('default');
-        this.tileDefault.animations.play('default', 12, true);
-        this.tileDefault.alpha = 1;
-        this.tileDefault.anchor.set(0.5, 1.3);
-        game.add.existing(this.tileDefault);
-
-        this.tileWalkReverse = game.add.tileSprite(realX, realY, 24, 24, 'walk_reverse');
-        this.tileWalkReverse.animations.add('walk_reverse');
-        this.tileWalkReverse.animations.play('walk_reverse', 12, true);
-        this.tileWalkReverse.alpha = 0;
-        this.tileWalkReverse.anchor.set(0.5, 1.3);
-        game.add.existing(this.tileWalkReverse);
-
-        this.tileDefaultReverse = game.add.tileSprite(realX, realY, 24, 24, 'default_reverse');
-        this.tileDefaultReverse.animations.add('default_reverse');
-        this.tileDefaultReverse.animations.play('default_reverse', 12, true);
-        this.tileDefaultReverse.alpha = 0;
-        this.tileDefaultReverse.anchor.set(0.5, 1.3);
-        game.add.existing(this.tileDefaultReverse);
+        this.tile = game.add.tileSprite(realX, realY, 24, 24, 'human');
+        this.tile.animations.add('walk', [0, 1, 2, 3, 4, 5]);
+        this.tile.animations.add('walk_reverse', [6, 7, 8, 9, 10, 11]);
+        this.tile.animations.add('default', [12, 13, 14]);
+        this.tile.animations.add('default_reverse', [18, 19, 20]);
+        this.tile.anchor.set(0.5, 1.3);
+        this.tile.animations.play('default', 12, true);
 
         game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(this.moveLeft, this);
         game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(this.moveRight, this);
@@ -93,22 +70,10 @@ export class Human {
     private runTween() {
         this.loadMove();
         this.isMoving = true;
-        this.game.add.tween(this.tileDefault.position).to({
+        this.game.add.tween(this.tile.position).to({
             x: PositionTransformer.getRealPosition(this.cell).x,
             y: PositionTransformer.getRealPosition(this.cell).y
         }, 1000, 'Linear', true).onComplete.add(this.isNotMoving, this);
-        this.game.add.tween(this.tileDefaultReverse.position).to({
-            x: PositionTransformer.getRealPosition(this.cell).x,
-            y: PositionTransformer.getRealPosition(this.cell).y
-        }, 1000, 'Linear', true);
-        this.game.add.tween(this.tileWalk.position).to({
-            x: PositionTransformer.getRealPosition(this.cell).x,
-            y: PositionTransformer.getRealPosition(this.cell).y
-        }, 1000, 'Linear', true);
-        this.game.add.tween(this.tileWalkReverse.position).to({
-            x: PositionTransformer.getRealPosition(this.cell).x,
-            y: PositionTransformer.getRealPosition(this.cell).y
-        }, 1000, 'Linear', true);
     }
 
     private isNotMoving() {
@@ -117,30 +82,20 @@ export class Human {
     }
 
     private loadMove() {
-        this.tileDefault.alpha = 0;
-        this.tileDefaultReverse.alpha = 0;
         if (this.isTop) {
-            this.tileWalkReverse.alpha = 1;
-            this.tileWalk.alpha = 0;
-            this.tileWalkReverse.scale.set(this.isLeft ? 1 : -1, 1);
+            this.tile.animations.play('walk_reverse', 12, true);
         } else {
-            this.tileWalk.alpha = 1;
-            this.tileWalkReverse.alpha = 0;
-            this.tileWalk.scale.set(this.isLeft ? 1 : -1, 1);
+            this.tile.animations.play('walk', 12, true);
         }
+        this.tile.scale.set(this.isLeft ? 1 : -1, 1);
     }
 
     private loadDefault() {
-        this.tileWalkReverse.alpha = 0;
-        this.tileWalk.alpha = 0;
         if (this.isTop) {
-            this.tileDefaultReverse.alpha = 1;
-            this.tileDefault.alpha = 0;
-            this.tileDefaultReverse.scale.set(this.isLeft ? 1 : -1, 1);
+            this.tile.animations.play('default_reverse', 12, true);
         } else {
-            this.tileDefault.alpha = 1;
-            this.tileDefaultReverse.alpha = 0;
-            this.tileDefault.scale.set(this.isLeft ? 1 : -1, 1);
+            this.tile.animations.play('default', 12, true);
         }
+        this.tile.scale.set(this.isLeft ? 1 : -1, 1);
     }
 }
--- src/game/game_state/Preload.ts
+++ src/game/game_state/Preload.ts
@@ -22,10 +22,11 @@ export default class Preload extends Phaser.State {
     }
 
     private loadGameImages() {
-        this.game.load.spritesheet('walk', 'assets/walk.png', 24, 24);
-        this.game.load.spritesheet('default', 'assets/default.png', 24, 24);
-        this.game.load.spritesheet('walk_reverse', 'assets/walk_reverse.png', 24, 24);
-        this.game.load.spritesheet('default_reverse', 'assets/default_reverse.png', 24, 24);
+        // this.game.load.spritesheet('walk', 'assets/walk.png', 24, 24);
+        // this.game.load.spritesheet('default', 'assets/default.png', 24, 24);
+        // this.game.load.spritesheet('walk_reverse', 'assets/walk_reverse.png', 24, 24);
+        // this.game.load.spritesheet('default_reverse', 'assets/default_reverse.png', 24, 24);
+        this.game.load.spritesheet('human', 'assets/human.png', 24, 24);
     }
 
     private loadFonts() {
