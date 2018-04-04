7425035543f6248c1b13ab4574604b2b31f3a480
pierallard <pierallard@gmail.com>
2018-04-04 17:10:39 +0200

    add random

--- src/game/human_stuff/TalkBubble.ts
+++ src/game/human_stuff/TalkBubble.ts
@@ -1,21 +1,35 @@
+const IMAGE_COUNT = 1;
+
 export class TalkBubble {
     private sprite: Phaser.Sprite;
     private parent: Phaser.TileSprite;
     private imageSprite: Phaser.Sprite;
-
-    constructor() {
-    }
+    private game: Phaser.Game;
 
     create(humanSprite: Phaser.TileSprite, game: Phaser.Game, group: Phaser.Group) {
+        this.game = game;
         this.parent = humanSprite;
-        this.sprite = game.add.sprite(this.parent.position.x, this.parent.position.y, 'bubble', 0, group);
+        this.sprite = game.add.sprite(
+            this.parent.position.x,
+            this.parent.position.y,
+            'bubble',
+            0,
+            group
+        );
         this.sprite.anchor.set(1, 35/this.sprite.height);
         group.add(this.sprite);
 
-        this.imageSprite = game.add.sprite(this.parent.position.x, this.parent.position.y, 'bubble_images', 0, group);
+        this.imageSprite = game.add.sprite(
+            this.parent.position.x,
+            this.parent.position.y,
+            'bubble_images',
+            0,
+            group
+        );
         this.imageSprite.anchor.set(1.2, 64/this.sprite.height);
         group.add(this.imageSprite);
 
+        this.switchImage();
         this.hide();
     }
 
@@ -34,5 +48,14 @@ export class TalkBubble {
         this.sprite.scale.x = this.parent.scale.x;
         this.imageSprite.position = this.parent.position;
         this.imageSprite.scale.x = this.parent.scale.x;
+    }
+
+    static getRandomFrame() {
+        return Math.floor(Math.random() * IMAGE_COUNT);
+    }
+
+    private switchImage() {
+        this.imageSprite.loadTexture(this.imageSprite.key, TalkBubble.getRandomFrame());
+        this.game.time.events.add(Phaser.Math.random(1, 3), this.switchImage, this)
     }
 }
